5dab64de59cbbd96a8fd275ac7aa50b70a4fd9a7
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2024-08-06 08:53:39 -0700

    added a heap problem - todays problem of the day on leeetcode

---
+++ problems/priority-queue/minimumPushes.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/
+# 3016. Minimum Number of Pushes to Type Word II
+
+from collections import Counter
+from heapq import heappush, heappop
+
+# a priority queue problem. Time: O(nlogn) for sots and the heap; Space: O(n)
+
+def minimumPushes(word: str) -> int:
+    countChars = [(n, char) for char, n in Counter(word).items()]
+    countChars.sort(key = lambda x: x[0])
+    # put 2-9 in a heap, sorted by numchars. When inserting, put key in hash with position 
+    heap = [0 for _ in range(2, 10)]
+    keypad = {} # char, num pushes 
+    while countChars: 
+        _, curChar = countChars.pop()
+        curKeypad = heappop(heap)
+        keypad[curChar] = curKeypad + 1
+        heappush(heap, curKeypad + 1)
+    return sum([keypad[x] for x in word])
