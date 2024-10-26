b3a49a8c8cb9fb7f64541d1ff44cde102b195a33
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2024-10-25 19:42:17 -0700

    update on remove subs

--- problems/strings/removeSubfolders.py
+++ problems/strings/removeSubfolders.py
@@ -37,3 +37,41 @@ class Solution:
             if not isSubFolderInFolders(x, folders): 
                 res.append(x)
         return res
+    
+
+
+# def getSubStringUpNotInclusive(x, i): 
+#     res = ""
+#     for j in range(i): 
+#         res += x[j]
+#     return res
+
+# def isSubFolderInFolders(x, res, folders): 
+#     for i in range(len(x) - 1, -1, -1): 
+#         if x[i] == "/" and getSubStringUpNotInclusive(x, i) in folders: return True
+#     return False
+
+# class Solution:
+#     def removeSubfolders(self, folder: List[str]) -> List[str]:
+#         folders = set(folder)
+#         res = []
+#         for x in folder: 
+#             if not isSubFolderInFolders(x, res, folders): 
+#                 res.append(x)
+#         return res
+    
+
+s = Solution()
+
+folder = ["/1/2/3", "/1", "/1/2"]
+# ["/1"]
+
+
+folder = ["1/2/3", "/2/3", "/3/4"]
+# ["1/2/3", "/2/3", "/3/4"]
+
+folder = ["/1/2/3", "/1/2/3/4", "/2/3/4", "/2/3"]
+# ["/1/2/3", "/2/3"]
+
+
+print(s.removeSubfolders(folder))
