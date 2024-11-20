8c495edb2a0ba409f4e484642ce0fb7c1c982415
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2024-11-19 16:08:43 -0800

    a quite hard one

---
+++ problems/math/largestComponentSize.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/largest-component-size-by-common-factor/
+# 952. Largest Component Size by Common Factor
+
+class Solution:
+    def largestComponentSize(self, nums: list[int]) -> int:
