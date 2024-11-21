672cc183be7cc23da86760ec1f2fe95ef5d38b4d
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2024-11-21 08:52:34 -0800

    some edits on binary search template

--- problems/binary-search/binarySearchTemplate.py
+++ problems/binary-search/binarySearchTemplate.py
@@ -2,18 +2,22 @@
 def f(x): 
     return x > 5
  
-# return the index of the earliest occurrance of when arr
+# Return the index of the earliest occurrence of when arr
 # Invariant: 
-# f(left) = 0
-# f(right) = 1
-# right will be 
-# - instantiate left and right outside the boudns of arr. It never 
-# gets evaluated directly
+#   f(left) = 0
+#   f(right) = 1
+#   right will be the first time f(x) = 1 
+# - instantiate left and right outside the bounds of arr,  
+# i.e. (-1 and n, respectively). 
+# -1 and n never gets evaluated directly. 
+# Key assumption: for arr, it's sorted in such a way that : 
+# f(0) = 0, f(1) = 0, .... f(k-1) = 0, f(k) = 1, f(k+1) = 1, ...
+# so we can then binary search the transition point k. 
 def binary_search(arr): 
     n = len(arr)
     left = -1
     right = n
-    while right - left > 1:             # 
+    while right - left > 1:         # right and left have to be 1 at least apart
         mid = (left + right)//2
         if f(mid): 
             right = mid    
