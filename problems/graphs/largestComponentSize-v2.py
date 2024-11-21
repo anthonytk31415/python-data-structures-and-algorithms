ea522104c7c299eb4a25e82a1ef4edf72290743e
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2024-11-20 21:42:57 -0800

    updated the largestcomp size file

--- problems/graphs/largestComponentSize-v2.py
+++ problems/graphs/largestComponentSize-v2.py
@@ -1,36 +1,63 @@
 from collections import Counter, defaultdict
-import math 
+from math import sqrt 
 
-class DSU:
-    def __init__(self, N):
-        self.p = list(range(N))
+# https://leetcode.com/problems/largest-component-size-by-common-factor/description/
+# 952. Largest Component Size by Common Factor
 
-    def find(self, x):
-        if self.p[x] != x:
-            self.p[x] = self.find(self.p[x])
-        return self.p[x]
+# An interesting union find problem. We decompose each number to its prime factorization. 
+# Then create an adjacency list for each prime, where in the list is the num. 
+# for each num in the prime list for a given prime p, we union adjacent numbers. 
+# At the end, we return the length of the longest parent. 
 
-    def union(self, x, y):
-        xr, yr = self.find(x), self.find(y)
-        self.p[xr] = yr
+# Time Complexity: O(n*sqrt(m)log(m)), dominated by the prime factorization of each num. So 
+# for each of the num, we have sqrt(m) divisors, each with log(m) recursive calls of the 
+# max number m in the worst case 
+# Space Complexity: O(nlogm) for n numbers and m prime factors for each number in the worst case
+
+def find(i, parent):
+    if i != parent[i]:
+        parent[i] = find(parent[i], parent)
+    return parent[i]
+
+def union(pu, pv, parent, rank):
+    if rank[pu] < rank[pv]:
+        pu, pv = pv, pu
+    parent[pv] = pu
+    rank[pu] += rank[pv]
+
+# keep cutting down n by 
+# runs in sqrt(n) log n time
+def prime_decomposition(n): 
+    for i in range(2, int(sqrt(n) + 1)): 
+        if n % i == 0: 
+            return prime_decomposition(n//i) | set([i])
+    return set([n])
 
 class Solution:
-    def primes_set(self,n):
-        for i in range(2, int(math.sqrt(n))+1):
-            if n % i == 0:
-                return self.primes_set(n//i) | set([i])
-        return set([n])
-
-    def largestComponentSize(self, A):
-        n = len(A)
-        UF = DSU(n)
+    def largestComponentSize(self, nums):
+        n = len(nums)
+        parent = [i for i in range(n)]
+        rank = [1]*n
         primes = defaultdict(list)
-        for i, num in enumerate(A):
-            pr_set = self.primes_set(num)
-            for q in pr_set: primes[q].append(i)
+        
+        # do prime_decomp on each num. 
+        # for each prime, add num to the adj list
+        for i, num in enumerate(nums):
+            i_prime_decomp = prime_decomposition(num)
+            for q in i_prime_decomp: 
+                primes[q].append(i)
 
-        for _, indexes in primes.items():
-            for i in range(len(indexes)-1):
-                UF.union(indexes[i], indexes[i+1])
+        # for each i and i + 1 pair of nums in the prime adj list, union the num indices
+        for primeList in primes.values():
+            for i in range(len(primeList)-1):
+                pu, pv = find(primeList[i], parent), find(primeList[i+1], parent)
+                if pu != pv: 
+                    union(pu, pv, parent, rank)
 
-        return max(Counter([UF.find(i) for i in range(n)]).values())
+        # return the length of the largest parent node
+        parentLength = Counter([find(i, parent) for i in range(n)])
+        return max(parentLength.values())
+        
+s = Solution()
+pset = s.prime_decomposition(24)
+print(pset)
