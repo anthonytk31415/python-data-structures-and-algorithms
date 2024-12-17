349a9e2e11e4eb17bcf55b60d8eb75404b4cff54
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2024-12-17 14:50:30 -0800

    strings add spaces

---
+++ problems/strings/addSpaces.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/adding-spaces-to-a-string/
+# 2109. Adding Spaces to a String
+
+# in my solution, I added sentinel values on spaces so you always take i, i+1
+
+# Time and space: O(n)
+
+class Solution:
+    def addSpaces(self, s: str, spaces: list[int]) -> str:
+        spaces = [0] + spaces + [len(s)]
+        res = []
+        for i in range(len(spaces)-1): 
+            start, end = spaces[i], spaces[i+1]
+            res.append(s[start:end])
+        return " ".join(res)
