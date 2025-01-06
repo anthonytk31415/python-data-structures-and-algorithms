ba95cc35404d12714e9c63766c98fb01aa0e20cc
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2025-01-05 16:40:42 -0800

    a dp using left and right tricks

---
+++ problems/dynamic-programming/minOperations.py
@@ -0,0 +1,30 @@
+# 1769. Minimum Number of Operations to Move All Balls to Each Box
+# https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
+
+
+# DP with left and right
+# time and space: O(n); can probably optimize
+
+def convert_to_array(boxes): 
+    res = []
+    for x in boxes: 
+        if x == "0": res.append(0)
+        else: res.append(1)
+    return res
+
+class Solution:
+    def minOperations(self, boxes: str) -> list[int]:
+        boxes = convert_to_array(boxes)
+        sum_r, sum_l, moves_l, moves_r, total_moves= [0]*len(boxes), [0]*len(boxes), [0]*len(boxes), [0]*len(boxes), [0]*len(boxes)
+        for i in range(1, len(sum_l)): 
+            sum_l[i] = boxes[i] + sum_l[i-1]
+            moves_l[i] = moves_l[i-1] + sum_l[i]
+        
+        for i in range(len(sum_r)-2, -1, -1): 
+            sum_r[i] = boxes[i] + sum_r[i+1]
+            moves_r[i] = moves_r[i+1] + sum_r[i]
+          
+        # build total
+        for i in range(len(total_moves)): 
+            total_moves[i] = moves_r[i] + moves_l[i]
+        return total_moves
