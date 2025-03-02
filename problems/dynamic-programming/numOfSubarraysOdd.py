6ffb07d7716546820c35d0cc21e973f576c892fb
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2025-03-02 09:21:24 -0800

    kadane solution to num odd subarrays

--- problems/dynamic-programming/numOfSubarraysOdd.py
+++ problems/dynamic-programming/numOfSubarraysOdd.py
@@ -1,4 +1,27 @@
 # https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/
 # 1524. Number of Sub-arrays With Odd Sum
 
-# can i solve with this dp? 
+# Dp implementation with ongoing prefix sum. Kadane inspo. 
+# linear time, constant space!
+
+class Solution:
+    def numOfSubarrays(self, arr: list[int]) -> int:
+        cumeEvens, cumeOdds = 0
+        priorEvens, priorOdds = 0
+        curEvens, curOdds = 0
+        for num in arr: 
+            if num % 2 == 0: 
+                curEvens = priorEvens + 1
+                curOdds = priorOdds                
+            else: 
+                curEvens = priorOdds
+                curOdds = priorEvens + 1                                
+
+            priorEvens, priorOdds = curEvens, curOdds
+            curEvens, curOdds = 0, 0
+            cumeEvens += priorEvens
+            cumeOdds += priorOdds
+            
+        return cumeOdds % (10**9 + 7)
+        
+        
