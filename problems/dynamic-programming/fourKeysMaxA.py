b2c0b364842deb02b7a8da7ba05f3e5eb879f345
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2025-04-07 19:56:56 -0700

    dp 4 keys

--- problems/dynamic-programming/fourKeysMaxA.py
+++ problems/dynamic-programming/fourKeysMaxA.py
@@ -1,11 +1,50 @@
 # https://leetcode.com/problems/4-keys-keyboard/?envType=study-plan-v2&envId=dynamic-programming-grandmaster
 # 651. 4 Keys Keyboard
 
+def determine_0(cur, prev):
+    maxEntry = prev[0]
+    for i in range(1, len(prev)):         
+        if prev[i][0] > maxEntry[0]: 
+            maxEntry = prev[i]            
+    (a, b, c) = maxEntry
+    cur[0] = (a + 1, b, c)
+    return 
 
+def determine_1(cur, prev):
+    maxEntry = prev[0]
+    for i in range(1, len(prev)):         
+        if prev[i][0] > maxEntry[0]: 
+            maxEntry = prev[i]            
+    (a, b, c) = maxEntry
+    cur[1] = (a, a, c)    
+    return 
+
+def determine_2(cur, prev):
+    maxEntry = prev[0]
+    for i in range(1, len(prev)):         
+        if prev[i][0] > maxEntry[0]: 
+            maxEntry = prev[i]            
+    (a, b, c) = maxEntry
+    cur[1] = (a, a, c)    
+    return 
+
+def determine_3(cur, prev):
+    return 
+
+def determine_max(prev): 
+    return max([x[0] for x in prev])
 
 def maxA(n): 
+    cur = [(0,0,0),(0,0,0),(0,0,0),(0,0,0)]
+    prev = [(0,0,0), (0,0,0), (0,0,0), (0,0,0)]
+
+    for _ in range(n): 
+        determine_0(cur, prev)
+        determine_1(cur, prev)
+        determine_2(cur, prev)
+        determine_3(cur, prev)
     
+        prev, cur = cur, [(0,0,0), (0,0,0), (0,0,0), (0,0,0)]
     
-    
-    
-    return 
+
+    return determine_max(prev)
