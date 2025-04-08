54a37649967faf39944b80df1b39f0a8a4a59b56
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2025-04-07 19:46:48 -0700

    dp 4 keys

---
+++ problems/dynamic-programming/fourKeysMaxA.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/4-keys-keyboard/?envType=study-plan-v2&envId=dynamic-programming-grandmaster
+# 651. 4 Keys Keyboard
+
+
+
+def maxA(n): 
+    
+    
+    
+    
+    return 
