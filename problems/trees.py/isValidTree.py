9634791826365ffd0571e07766464252a211b688
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2025-05-03 13:02:23 -0700

    valid tree written

---
+++ problems/trees.py/isValidTree.py
@@ -0,0 +1,44 @@
+# edges = [edge]
+# edge = [a, b]
+
+# Input: n = 5, edges = [[0,1],[0,2],[0,3],[1,4]]
+# 0: 1,2,3
+# 1: 0, 4
+# 2: 0
+# 3: 0
+
+
+# write a function: is this a tree
+# - can have multiple children 
+# - only one parent 
+
+# build an adjacency list 
+
+# critique on time saving operations
+
+from collections import defaultdict
+
+def isValidTree(n, edges): 
+    # time saving operations
+    num_edges = len(edges)
+    if num_edges != n-1: 
+        return False
+    
+    def createAdjList(edges):
+        adjList = defaultdict(list)
+        for u, v in edges: 
+            adjList[u].append(v)         
+            adjList[v].append(u)         
+        return adjList
+
+    def dfs(start): 
+        visited.add(start)
+        for children in adjList[start]: 
+            if children not in visited: 
+                dfs(children, visited, adjList)
+    
+    adjList = createAdjList(edges)
+    start = 0
+    visited = set()
+    dfs(start)
+    return len(visited) == n 
