8e27676194cdd34b75be039f929577db2536ce25
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2025-06-12 22:27:25 -0700

    diff array

---
+++ problems/prefix-sum/isZeroArray.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/zero-array-transformation-i/?envType=company&envId=google&favoriteSlug=google-thirty-days
+# 3355. Zero Array Transformation I
+
+# a google problem
+
+# use a diff array to update your diff_array in O(1) time. 
+# make sure you do update on j + 1 -= 1. Think about why. 
+
+# O(n) Time and space
+
+from typing import List 
+
+
+def get_diff_array(n, queries): 
+    diff = [0]*n
+    for i, j in queries: 
+        diff[i] += 1
+        if j + 1 < n: 
+            diff[j+1] -= 1
+    return diff
+
+def is_valid(nums, diff): 
+    tracker = 0
+    for i in range(len(nums)): 
+        tracker += diff[i]
+        if nums[i] > tracker: 
+            return False
+    return True
+
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n = len(nums)
+        diff = get_diff_array(n, queries)
+        return is_valid(nums, diff)
