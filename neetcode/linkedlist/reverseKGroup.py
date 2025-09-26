573f667ebcac8ec13e4393c6cf84cb01c235cb3a
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2025-09-26 10:45:50 -0700

    updated

--- neetcode/linkedlist/reverseKGroup.py
+++ neetcode/linkedlist/reverseKGroup.py
@@ -1,6 +1,6 @@
 # https://neetcode.io/problems/reverse-nodes-in-k-group?list=neetcode150
 
-# great problem..
+# great problem... apply functions in O(n) time.
 
 
 # Definition for singly-linked list.
